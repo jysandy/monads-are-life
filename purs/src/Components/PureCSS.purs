599a412a761abdf4eae406834fe4a81fdd18967e
Sandilya Jandhyala <jysandilya@gmail.com>
2020-05-19 11:43:33 +0530

    Make the column component a React component

--- purs/src/Components/PureCSS.purs
+++ purs/src/Components/PureCSS.purs
@@ -5,7 +5,6 @@ import Prelude
 import React.Basic (JSX, createComponent, makeStateless)
 import React.Basic.DOM as R
 
-
 row :: Array JSX -> JSX
 row =
   makeStateless (createComponent "Row")
@@ -26,16 +25,18 @@ unitClassName numerator denominator = "pure-u-" <> show numerator <> "-" <> show
 
 -- column 1 3 => a column that takes up 1/3 of the width.
 column :: Int -> Int -> Array JSX -> JSX
-column numerator denominator children =
-  R.div
-    { className: unitClassName numerator denominator
-    , children: children
-    }
+column numerator denominator =
+  makeStateless (createComponent $ "Column-" <> show numerator <> "-" <> show denominator)
+    $ \children ->
+        R.div
+          { className: unitClassName numerator denominator
+          , children: children
+          }
 
 text :: String -> Array JSX
 text s = [ R.text s ]
 
-table :: forall r.{ header :: Array String, rows :: Array (Array String) | r } -> JSX
+table :: forall r. { header :: Array String, rows :: Array (Array String) | r } -> JSX
 table =
   makeStateless (createComponent "Table")
     $ \{ header, rows } ->
@@ -47,4 +48,4 @@ table =
               ]
           }
   where
-  makeTR row' = R.tr_ $ map (R.td_ <<< text ) row'
+  makeTR row' = R.tr_ $ map (R.td_ <<< text) row'
